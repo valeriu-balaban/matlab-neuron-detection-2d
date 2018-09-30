11dd71fbcbcd9a5ea71a9fae6c7c42f57271de7d
Pen Pen <silencezp97@gmail.com>
2018-09-30 15:12:49 +0800

    update test step for synapse detection

--- FindNeurons_test.m
+++ FindNeurons_test.m
@@ -9,7 +9,7 @@ THETA_THRESHOLD = 0.6;
 
 %% find neurons
 
-[position, radius, BW] = FindNeurons(img , RADIUS_LIST, EDGE_WIDTH, THETA_THRESHOLD);
+[position, radius, BW] = FindNeurons(img, RADIUS_LIST, EDGE_WIDTH, THETA_THRESHOLD);
 
 %% assign possibility and direction for synapse
 BW_thin = bwmorph(BW, 'thin', 20);
@@ -18,18 +18,41 @@ sigma = 8;
 [U, V, theta] = neurite_vector(BW, sigma);
 
 theta = theta .* BW_thin;
-%% Synapse detection
-% degree and distance threshold when connecting broken synapse
+
+%% Astar algorithm for synapse detection
+% draw neurons
+draw_circles(position, radius, img);
+
+% label
+for i = 1:length(radius)
+    text(position(i,1),position(i,2),int2str(i),'FontSize',10,'Color','red');
+end
+% find
 theta_thre = 15;
-fill_gap = 0;
-[connected, synapse] = synapse_detection(BW, BW_thin, position,...
-                                                radius, theta, theta_thre, fill_gap);
+fill_gap = 20;
+
+%init
+num = size(position, 1);
+connected = zeros(num, num);
+synapse = cell(num, num);
+
+parfor i = 1:num
+    [connected(i, :), synapse(i, :)] = Astar_Synapse(BW_thin, i, radius, ...
+                                    position, theta, fill_gap, theta_thre); 
+end
 
 % compute width of synapses
 breadth = synapse_breadth(synapse, BW);
-%% show
 
-% draw neurons
-draw_circles(position, radius, img);
 % draw synapses
 draw_synapse(connected, synapse, breadth);
+
+ %% Synapse detection
+% % degree and distance threshold when connecting broken synapse
+% theta_thre = 15;
+% fill_gap = 0;
+% [connected, synapse] = synapse_detection(BW, BW_thin, position,...
+%                                                 radius, theta, theta_thre, fill_gap);
+% 
+% % compute width of synapses
+% breadth = synapse_breadth(synapse, BW);
