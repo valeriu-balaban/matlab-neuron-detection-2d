f9a8b4f5b2dd006c8a78766381910492bc4a2834
Pen Pen <silencezp97@gmail.com>
2018-10-29 20:03:01 +0800

    adjust for loops: delete previous data file and result file.

--- matlab_c_three_image.m
+++ matlab_c_three_image.m
@@ -1,19 +1,26 @@
-load 'three_image.mat'
-IMG_STR = 'neurons-step1-result.tif';
+load '~/downloads/three_image.mat'
+IMG_STR = '~/downloads/three_image.tif';
 
 num_image = 3;
 A = cell(num_image, 1);
 PATHS = A;
 
 for image_index = 1:num_image
-    
+    %% remove previous files
+    if exist('data.txt', 'file')
+        delete('data.txt');
+    end
+    if exist('result.txt', 'file')
+        delete('result.txt');
+    end
+    %% compute matrix
     P = cell2mat(NeuronLocations{image_index});
     % Change coordinates from x, y to row, col
     TMP = P(:, 1);
     P(:, 1) = P(:, 2);
     P(:, 2) = TMP;
 
-    I = imread(IMG_STR, image_index * 10 - 9);
+    I = imread(IMG_STR, image_index);
 
     [A{image_index}, PATHS{image_index}] = TraceConnections(I, P, 15);
 
